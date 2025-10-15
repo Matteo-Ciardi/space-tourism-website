7cc7b51cefd98a71208eeb730ed3df3bc581591b
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-16 00:02:39 +0200

    aggiunti link alla navbar principale

--- src/components/MainNavbarDesktop.jsx
+++ src/components/MainNavbarDesktop.jsx
@@ -1,13 +1,30 @@
+import { NavLink, Link } from "react-router-dom"
 
+const navLinks = [
+    { path: "/", label: "HOME" },
+    { path: "/destination", label: "DESTINATION" },
+    { path: "/crew", label: "CREW" },
+    { path: "/technology", label: "TECHNOLOGY" }
+]
 
 const MainNavbarDesktop = () => {
 
     return (
-        <>
-            <div>
-                SONO LA NAVBAR
+        <nav>
+            <div className="logo">
+                <Link to={"/"}>--LOGO--</Link>
             </div>
-        </>
+
+            <ul>
+                {navLinks.map(link => (
+                    <li key={link.path}>
+                        <NavLink to={link.path}>
+                            {link.label}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
 
