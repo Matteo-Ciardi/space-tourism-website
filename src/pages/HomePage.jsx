f0c8e453da2205ece388a8543127adb819fbf98c
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-16 23:27:21 +0200

    aggiunto button in home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,11 +16,22 @@ const HomePage = () => {
         <div className="main-content">
             <div className="hero">
                 <div className="texts">
-                    <span className="text-preset-8">SO, YOU WANT TO TRAVEL TO</span>
-                    <span className="big-title">SPACE</span>
+
+                    <span className="text-preset-8">
+                        SO, YOU WANT TO TRAVEL TO
+                    </span>
+
+                    <span className="big-title">
+                        SPACE
+                    </span>
+
                     <p className="home-content">
                         Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
                     </p>
+
+                    <button className="home-button">
+                        EXPLORE
+                    </button>
                 </div>
             </div>
         </div>
